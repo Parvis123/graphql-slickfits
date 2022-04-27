cd86f6e0257cb9a890f62b34bb5bbf05bc46ef2d
Michael <michael_parvis@hotmail.com>
2022-04-27 19:01:07 +0100

    backend working, header done

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,33 @@
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+
+const databaseURL =
+  process.env.DATABASE_URL ||
+  'monogodb:mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // how long should they remain signed in?
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+    // TODO add data seeding here
+  },
+  lists: createSchema({
+    // Schema items go in here
+  }),
+  ui: {
+    // TODO change this for roles
+    isAccessAllowed: () => true,
+  },
+  // TODO add session values here
+});
