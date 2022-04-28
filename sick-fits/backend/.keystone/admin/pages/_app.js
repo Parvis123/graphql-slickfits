49611d4f0313ae49d9312c96effcad21f68fdcb5
Michael <michael_parvis@hotmail.com>
2022-04-28 16:43:11 +0100

    graphql queries working, added dummy data to backend

--- sick-fits/backend/.keystone/admin/pages/_app.js
+++ sick-fits/backend/.keystone/admin/pages/_app.js
@@ -198,7 +198,7 @@ export default function App({ Component, pageProps }) {
     <Core>
       <KeystoneProvider
         adminConfig={adminConfig}
-        adminMetaHash="vn0sgv"
+        adminMetaHash="1vlxjj2"
         fieldViews={fieldViews}
         lazyMetadataQuery={lazyMetadataQuery}
       >
